05e55fd15cb690bca32c208da0f05b7d08cfb20a
Esa Aaltonen <etaalton@gmail.com>
2024-08-07 10:19:18 +0300

    New launch scripts for bunker/rplidar

---
+++ launch/bunker_rplidar_mapper_launch.py
@@ -0,0 +1,109 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        SetEnvironmentVariable(name='TURTLEBOT3_MODEL', value='waffle'),
+        SetEnvironmentVariable(name='LDS_MODEL', value='LDS-01'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('sllidar_ros2'),
+                    'launch',
+                    'sllidar_a3_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'serial_port': '/dev/ttyUSB1',
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('rf2o_laser_odometry'),
+                    'launch',
+                    'rf2o_laser_odometry.launch.py'
+                ])
+            ]),
+            
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('turtlebot3_bringup'),
+                    'launch',
+                    'robot.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'use_sim_time': 'False',
+            }.items()
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'odom', 'base_footprint']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_footprint', 'base_link']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        ),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('nav2_bringup'),
+                    'launch',
+                    'navigation_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'use_sim_time': 'False',
+                'autostart': 'True',
+                'map': '/home/vesa/map2.yaml',
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('slam_toolbox'),
+                    'launch',
+                    'online_async_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                #'use_sim_time': 'False',
+            }.items()
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', '/opt/ros/humble/share/nav2_bringup/rviz/nav2_default_view.rviz']
+        ),
+
+
+    ])
---
+++ launch/bunker_rplidar_navigation_launch.py
@@ -0,0 +1,98 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    map_path = LaunchConfiguration('map_path')
+    map_launch_arg = DeclareLaunchArgument(
+        'map',
+        default_value="/home/agilex/map1.yaml"
+    )
+
+    return LaunchDescription([
+        SetEnvironmentVariable(name='TURTLEBOT3_MODEL', value='waffle'),
+        SetEnvironmentVariable(name='LDS_MODEL', value='LDS-01'),
+
+        map_launch_arg,
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('sllidar_ros2'),
+                    'launch',
+                    'sllidar_a3_launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'serial_port': '/dev/ttyUSB1',
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('rf2o_laser_odometry'),
+                    'launch',
+                    'rf2o_laser_odometry.launch.py'
+                ])
+            ]),
+            
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('turtlebot3_bringup'),
+                    'launch',
+                    'robot.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'use_sim_time': 'False',
+            }.items()
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'odom', 'base_footprint']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_footprint', 'base_link']
+        ),
+        
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        ),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('turtlebot3_navigation2'),
+                    'launch',
+                    'navigation2.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'use_sim_time': 'False',
+                'autostart': 'True',
+                'map': map_path,
+            }.items()
+        ),
+
+
+    ])
